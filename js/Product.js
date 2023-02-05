d507c44ce77637e51aea13f2d8aaaf66fc64c19f
Hoo Jia Yi <jiayi030315@gmail.com>
2023-02-05 21:03:01 +0800

    Add js code for interaction of size button and change image

---
+++ js/Product.js
@@ -0,0 +1,28 @@
+//This is for showing different product images on product page
+const productImages = document.querySelectorAll(".product-images img");
+const productActiveImage = document.querySelector(".active-image .active");
+
+let activeImage = 0;//default selected images which is the first one
+
+productImages.forEach((item, i) => {
+    item.addEventListener('click', () =>{
+        productImages[activeImage].classList.remove('active');
+        item.classList.add('active');
+        productActiveImage.src = item.src;
+        activeImage = i;
+    })
+})
+
+
+//This is for selecting radio size button
+
+const sizebtn = document.querySelectorAll('.size-radio-button');
+let checkedbtn = 0;//default selected button which is the first one
+
+sizebtn.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        sizebtn[checkedbtn].classList.remove('check');
+        item.classList.add('check');
+        checkedbtn = i;
+    })
+})
