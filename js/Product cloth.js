b13cb50f392df4aa50b62042436a89b8c472f407
Hoo Jia Yi <jiayi030315@gmail.com>
2023-02-07 23:46:56 +0800

    Change the code because the desription is changed from sleevelength to product length

--- js/Product cloth.js
+++ js/Product cloth.js
@@ -51,7 +51,7 @@ productdiscount.innerHTML = localStorage.getItem("discount");
 
 //Change the product desc
 productdesc[0].children[1].innerHTML = localStorage.getItem("color");
-productdesc[1].children[1].innerHTML = localStorage.getItem("sleevelength");
+productdesc[1].children[1].innerHTML = localStorage.getItem("length");
 productdesc[2].children[1].innerHTML = localStorage.getItem("fitType");
 productdesc[3].children[1].innerHTML = localStorage.getItem("material");
 productdesc[4].children[2].innerHTML = localStorage.getItem("careInstruction");
