a522b406b282251066751b1fb8ed0f560bfce92a
Hoo Jia Yi <jiayi030315@gmail.com>
2023-02-08 20:32:39 +0800

    Add js code and edit html code to enable the tranferring of books detail and able to display in individual product page

--- js/book.js
+++ js/book.js
@@ -1,20 +1,73 @@
 function change() {
-    let results = Array.from(document.querySelectorAll('.product-container > div'));
-    // Hide all results
-    results.forEach(function(result) {
-      result.style.display = 'none';
+    let results = document.querySelectorAll('.product-container li');
+    let genre = document.querySelectorAll('.filters__list li');
+    let filtered = [];
+    // Hide all results(Products)
+    results.forEach((item, i) => {
+      item.style.display = 'none';
     });
-    // Filter results to only those that meet ALL requirements:
-    Array.from(document.querySelectorAll('.filter__list input[id]:checked'), function(input) {
-      const attrib = input.getAttribute('id');
-      results = results.filter(function(result) {
-        return result.classList.contains(attrib);
+    // check each of the checkbox, if it is checked, then add it into the array
+    genre.forEach((item,i) => {
+      if (item.children[0].checked){
+        filtered.push(item.children[0].id);
+      }
+    })
+    // For each of the result(Product), if the classname is in the array(it is checked), then display it
+    results.forEach((item, i) => {
+      if (filtered.includes(item.className)){
+        item.style.display = "block";
+      }
+    });
+    // if the length of array is 0 which means that nothing is being filtered, then we show all of the results(products)
+    if (filtered.length == 0){
+      results.forEach((item, i) => {
+        item.style.display = 'block';
       });
-    });
-    // Show those filtered results:
-    results.forEach(function(result) {
-      result.style.display = 'block';
-    });
+    }
   }
-  
-  change();
+
+
+  //for  discount books products pages
+const Discountbookproducts = document.querySelectorAll(".discount .product-image a img");
+
+Discountbookproducts.forEach((item, i) => {
+  item.addEventListener('click', () =>{
+    let parent = item.parentElement.parentElement;
+    var src1 = item.src;
+    var name = parent.nextElementSibling.children[0].innerHTML;
+    var price = parent.nextElementSibling.children[1].children[0].innerHTML;
+    var Originalprice = parent.nextElementSibling.children[1].children[1].innerHTML;
+    var discount = parent.children[0].innerHTML;
+    var Nopages = parent.nextElementSibling.children[2].children[0].innerHTML;
+    var bookdesc = parent.nextElementSibling.children[2].children[1].innerHTML;
+    localStorage.setItem("src1",src1);
+    localStorage.setItem("name",name);
+    localStorage.setItem("price",price);
+    localStorage.setItem("Originalprice",Originalprice);
+    localStorage.setItem("discount",discount);
+    localStorage.setItem("Nopages",Nopages);
+    localStorage.setItem("bookdesc",bookdesc);
+  })
+})
+
+
+//for Popular books products pages
+const Popularbookproducts = document.querySelectorAll(".product-image a img");
+
+Popularbookproducts.forEach((item, i) => {
+  item.addEventListener('click', () =>{
+    let parent = item.parentElement.parentElement;
+    var src1 = item.src;
+    var name = parent.nextElementSibling.children[0].innerHTML;
+    var price = parent.nextElementSibling.children[1].children[0].innerHTML;
+    var Nopages = parent.nextElementSibling.children[2].children[0].innerHTML;
+    var bookdesc = parent.nextElementSibling.children[2].children[1].innerHTML;
+    localStorage.setItem("src1",src1);
+    localStorage.setItem("name",name);
+    localStorage.setItem("price",price);
+    localStorage.setItem("Originalprice","");
+    localStorage.setItem("discount","");
+    localStorage.setItem("Nopages",Nopages);
+    localStorage.setItem("bookdesc",bookdesc);
+  })
+})
