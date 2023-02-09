971ec39731734a0203604fab4968031f5e821078
Hoo Jia Yi <jiayi030315@gmail.com>
2023-02-09 20:55:35 +0800

    Add the js code for signin page

--- js/Signin.js
+++ js/Signin.js
@@ -1,20 +1,13 @@
 const submitButton = document.querySelector(".submit-btn");
-const username = document.querySelector("#name");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
-const phone = document.querySelector("#phone");
 const APIKEY = "63b95c28969f06502871ac12";
-const emaillist = [];
 
 var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 $(".submit-btn").on("click", function(e) {
     e.preventDefault();
-    
-    if (username.value.length < 3) {
-        alert("Name must be at least 3 letters long");
-    }
-    else if (!email.value.length) {
+    if (!email.value.length) {
         alert("Please enter your email");
     }
     else if (!email.value.match(validRegex)) {
@@ -23,16 +16,10 @@ $(".submit-btn").on("click", function(e) {
     else if (password.value.length < 8 || !password.value.length) {
         alert("Password should at least 8 letters long");
     }
-    else if (!phone.value.length) {
-        alert("Please enter your phone number");
-    }
-    else if (phone.value.length != 8 || isNaN(phone.value)) {
-        alert("Invalid number, please enter a valid phone number with 8 digits");
-    }
-    else {
-
+    else{
+        $(".lottieloading2").show();
         let settings = {
-            "async": false,
+            "async": true,
             "crossDomain": true,
             "url": "https://interactivedev-333e.restdb.io/rest/profile",
             "method": "GET",
@@ -42,49 +29,31 @@ $(".submit-btn").on("click", function(e) {
               "cache-control": "no-cache"
             },
         }
-
+    
         $.ajax(settings).done(function (response) {
-            for (var i = 0; i < response.length; i++) {
-                emaillist.push(response[i].email);
-            }
-            post();
+            $(".lottieloading2").hide();
+            setTimeout(function () {
+                for (var i = 0; i < response.length; i++) {
+                    if(response[i].email == email.value){
+                        if(response[i].password == password.value){
+                            localStorage.setItem("username",response[i].name);
+                            window.location.href = "index.html";
+                            break;
+                        }  
+                        else{
+                            alert("You have entered a wrong password");
+                            break;
+                        }
+                    }
+                    else{
+                        if (i+1 == response.length){
+                            alert("You have entered a wrong email");
+                        }
+                    }
+                }
+             }, 110)
+
         })
     }
+
 })
-
-
-function post(){
-    if (emaillist.includes(email.value)){
-        alert("The email have been registered with another account");
-    }
-    else{
-        alert("lottieshowing");
-        $(".lottieloading").show();
-
-        let jsondata = {
-            "name": username.value,
-            "email": email.value,
-            "password": password.value,
-            "phone": phone.value
-        }
-
-        let settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://interactivedev-333e.restdb.io/rest/profile",
-            "method": "POST",
-            "headers": {
-                "content-type": "application/json",
-                "x-apikey": APIKEY,
-                "cache-control": "no-cache"
-            },
-            "processData": false,
-            "data": JSON.stringify(jsondata),
-        }
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-            $(".lottieloading").hide();
-            $(".lottiecompleted").show().fadeOut(3000);
-        })
-    }
-}
